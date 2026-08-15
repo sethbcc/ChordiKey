c895b4a5069d6700d0fb99e86f2954ae4bc2ae3f
sethbcc <chuachunsethbenjamin@gmail.com>
2026-08-15 14:02:01 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,29 +2,7 @@
 // 1. FIREBASE AUTHENTICATION INITIALIZATION
 // =========================================================
 // Replace these placeholders with your config from Firebase Console:
-<script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.17.1/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.17.1/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyAHYAFwNWJRQJHo2un3di6FAf6s2sAyUMY",
-    authDomain: "chordikey.firebaseapp.com",
-    projectId: "chordikey",
-    storageBucket: "chordikey.firebasestorage.app",
-    messagingSenderId: "200443484160",
-    appId: "1:200443484160:web:8c0e892f9811caea5620e5",
-    measurementId: "G-B8DKLCTZ9C"
-  };
-
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
-</script>
 
 // Profile Page DOM Elements
 const profileLoggedOut = document.getElementById('profile-logged-out');
